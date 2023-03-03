b5fa3bb53a5a85017a960dba0faa32361d41f9d6
applepig85 <applepig85@gmail.com>
2023-03-04 00:00:31 +0900

    init

---
+++ src/main/java/com/applepig/webfluxpractice/WebfluxPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.applepig.webfluxpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applepig/webfluxpractice/WebfluxPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applepig.webfluxpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
