7b3fd8cb595c0ae31739d28b02ea0463a143a237
applepig85 <applepig85@gmail.com>
2023-03-04 21:27:38 +0900

    subscribe teset

--- src/main/java/com/applepig/webfluxpractice/controller/ExperimentController.java
+++ src/main/java/com/applepig/webfluxpractice/controller/ExperimentController.java
@@ -33,14 +33,29 @@ public class ExperimentController {
     }
 
     /*
-     * expected:
-     * actual:
+     * expected: get3는 즉시 응답하되, get3의 로그와 /person의 로그가 출력된다.
+     * actual: 전자만 출력된다.
      */
     @GetMapping("/mono/2")
     private Person get2() throws InterruptedException{   
         System.out.println("get2 begin");
         Mono<Person> result = WebClient.create("http://localhost:8080").get().uri("/person").retrieve().bodyToMono(Person.class);
         System.out.println("get2 end");
+        return Person.builder().age(20).name("default").build();
+    }
+
+    /*
+     * expected: get3는 즉시 응답하되, get3의 로그와 /person의 로그가 출력된다.
+     * actual: 상동
+     */
+    @GetMapping("/mono/3")
+    private Person get3() throws InterruptedException{   
+        System.out.println("get3 begin");
+        Mono<Person> result = WebClient.create("http://localhost:8080").get().uri("/person").retrieve().bodyToMono(Person.class);
+        System.out.println("get3 sub begin");
+        result.subscribe(); // 이것이 실제 위 호출을 가능하게 한다.
+        System.out.println("get3 sub end");
+        System.out.println("get3 end");
         return Person.builder().age(20).name("default").build();
     }
 
