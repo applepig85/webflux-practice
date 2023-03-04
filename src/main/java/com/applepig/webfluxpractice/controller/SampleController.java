2ea4086c103a5b299f1dd85f535568c501d47427
applepig85 <applepig85@gmail.com>
2023-03-04 21:12:06 +0900

    experiment: flux

--- src/main/java/com/applepig/webfluxpractice/controller/SampleController.java
+++ src/main/java/com/applepig/webfluxpractice/controller/SampleController.java
@@ -1,10 +1,14 @@
 package com.applepig.webfluxpractice.controller;
 
+import java.time.Duration;
+import java.util.stream.Stream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.applepig.webfluxpractice.data.Person;
+import com.fasterxml.jackson.databind.deser.impl.ExternalTypeHandler.Builder;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
@@ -31,6 +35,35 @@ public class SampleController {
         Mono<Person> result = WebClient.create("http://localhost:8080").get().uri("/person").retrieve().bodyToMono(Person.class);
         System.out.println("get end");
         return result;
+    }
+
+    @GetMapping("/get2")
+    private Person get2() throws InterruptedException{   
+        System.out.println("get2 begin");
+        Mono<Person> result = WebClient.create("http://localhost:8080").get().uri("/person").retrieve().bodyToMono(Person.class);
+        System.out.println("get2 end");
+        return Person.builder().age(20).name("default").build();
+    }
+
+    @GetMapping("/exp/flux")
+    private Flux<Person> expFlux() throws InterruptedException{   
+        System.out.println("expFlux begin");
+        Flux<Person> result = 
+            WebClient.create("http://localhost:8080")
+                    .get()
+                    .uri("/person/flux")
+                    .retrieve()
+                    .bodyToFlux(Person.class);
+        System.out.println("expFlux end");
+        return result;
+    }
+    @GetMapping("/person/flux")
+    private Flux<Person> personFlux() throws InterruptedException{   
+        System.out.println("personFlux begin");
+        Stream<Integer> stream = Stream.iterate(0, i -> i + 1); // Java8의 무한 Stream
+        System.out.println("personFlux end");
+        return Flux.fromStream(stream.limit(20)).delayElements(Duration.ofSeconds(1))
+                .map(i -> new Person(i,"hello, user"+String.valueOf(i) )  );
     }
 
 }
--- src/main/java/com/applepig/webfluxpractice/data/Person.java
+++ src/main/java/com/applepig/webfluxpractice/data/Person.java
@@ -1,6 +1,7 @@
 package com.applepig.webfluxpractice.data;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Person {
     int age;
     String name;
